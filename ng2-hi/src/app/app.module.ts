11f1e354275646262a49ab184c5e4f5761791486
wanghao <wanghao211939@sogou-inc.com>
2017-03-20 10:36:26 +0800

    add highlight

--- ng2-hi/src/app/app.module.ts
+++ ng2-hi/src/app/app.module.ts
@@ -4,10 +4,12 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { HighlightDirective } from './highlight.directive';
 
 @NgModule({
   declarations: [
-    AppComponent  // 引入工作组件
+    AppComponent,  // 引入工作组件
+    HighlightDirective
   ],
   imports: [
     BrowserModule,
--- ng2-hi/src/app/highlight.directive.ts
+++ ng2-hi/src/app/highlight.directive.ts
@@ -1,1 +1,10 @@
-ng2-der
+import { Directive, ElementRef, Renderer } from '@angular/core';
+
+@Directive({
+    selector: '[highlight]',
+})
+export class HighlightDirective {
+    constructor(el: ElementRef, render: Renderer) {
+        render.setElementStyle(el.nativeElement, 'backgroundColor', 'yellow');
+    }
+}
