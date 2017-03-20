0f86ee6ba424d2347629200f03a30a7acf8fbe14
wanghao <wanghao211939@sogou-inc.com>
2017-03-20 10:10:22 +0800

    finish ngif

--- ng2-hi/src/app/app.component.ts
+++ ng2-hi/src/app/app.component.ts
@@ -8,6 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class AppComponent implements OnInit{
   title = 'hi，ng2！';
   private greeting:string;
+  private isShowMore: boolean;
   constructor() {
   }
 
